6bfabb35bcd19d53e7241831dff1b59d0d48d32f
Cwhiting213 <41601452+Cwhiting213@users.noreply.github.com>
2022-04-02 03:03:39 -0400

    deploy

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "redditclone24e283ce": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "redditclone": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "redditimages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
